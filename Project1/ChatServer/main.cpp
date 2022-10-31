d89b98a0521bfd0bf27768a3880a158a2fa8ebdb
Diego Bastos <bastosdiegol@gmail.com>
2022-10-31 00:30:22 -0400

    Added GoogleProtobuf lib and my own authcomm.proto file

--- Project1/ChatServer/main.cpp
+++ Project1/ChatServer/main.cpp
@@ -1,13 +1,40 @@
 #include "ChatServer.h"
 #include "ChatMessageProtocol.h"
+#include "authcomm.pb.h"
 #include "Buffer.h"
 #include <iostream>
 
 int main(int argc, char** argv) {
 
-	ChatServer cs;
-	cs.StartUp();
-	cs.Shutdown();
+	auth::ChatServerRequest serverRequest;
+	serverRequest.set_type(auth::ChatServerRequest::AUTHENTICATE);
+	serverRequest.set_id(1);
+	serverRequest.set_email("bastosdiegol@gmail.com");
+	serverRequest.set_password("123456");
+
+	std::string serializedString;
+	serverRequest.SerializeToString(&serializedString);
+
+	std::cout << serializedString << std::endl;
+	for (int idxString = 0; idxString < serializedString.length(); idxString++) {
+		printf("%02X ", serializedString[idxString]);
+	}
+	printf("\n");
+
+	auth::ChatServerRequest deserializedServerRequest;
+	bool result = deserializedServerRequest.ParseFromString(serializedString);
+	if (!result) {
+		std::cout << "Failed to parse server request" << std::endl;
+	}
+	std::cout << "Parsing successful" << std::endl;
+	std::cout << "Request Type: " << deserializedServerRequest.type() << std::endl;
+	std::cout << "ID: " << deserializedServerRequest.id() << std::endl;
+	std::cout << "E-mail: " << deserializedServerRequest.email() << std::endl;
+	std::cout << "Password: " << deserializedServerRequest.password() << std::endl;
+
+	//ChatServer cs;
+	//cs.StartUp();
+	//cs.Shutdown();
 
 	return 0;
 }
